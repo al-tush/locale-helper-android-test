4561e7f74342c5ccc3ea42947c87df53bdeb9ccf
Alexey Tushurashvili <a.tushurashvili@corp.mail.ru>
2020-07-29 19:17:49 +0300

    com.akexorcist:localization:1.2.5 works ok on app start

--- app/src/main/java/com/example/testlocaleapp/MainActivity.kt
+++ app/src/main/java/com/example/testlocaleapp/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.example.testlocaleapp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Button
-import com.zeugmasolutions.localehelper.LocaleAwareCompatActivity
+import com.akexorcist.localizationactivity.ui.LocalizationActivity
 import java.util.*
 
-class MainActivity : LocaleAwareCompatActivity() {
+class MainActivity : LocalizationActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button_first).setOnClickListener {
-            updateLocale(Locale("ru", "RU"))
+            setLanguage(Locale("ru", "RU"))
         }
     }
 }
--- app/src/main/java/com/example/testlocaleapp/TestApp.kt
+++ app/src/main/java/com/example/testlocaleapp/TestApp.kt
@@ -1,6 +1,16 @@
 package com.example.testlocaleapp
 
-import com.zeugmasolutions.localehelper.LocaleAwareApplication
+import android.app.Application
+import android.content.Context
+import com.akexorcist.localizationactivity.core.LocalizationApplicationDelegate
+import com.akexorcist.localizationactivity.ui.LocalizationApplication
+import java.util.*
 
-class TestApp: LocaleAwareApplication() {
+class TestApp: Application() {
+    private val localizationDelegate = LocalizationApplicationDelegate()
+
+    override fun attachBaseContext(base: Context) {
+        localizationDelegate.setDefaultLanguage(base, Locale.ENGLISH)
+        super.attachBaseContext(localizationDelegate.attachBaseContext(base))
+    }
 }
